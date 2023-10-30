43501db7351c0857157fc9ced7377ecb5cabfb4d
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2023-10-30 03:42:49 -0700

    query provider

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import "react-native-gesture-handler";
 import { StyleSheet, View } from "react-native";
 import AppNavigator from "./src/navigation/AppNavigator";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App() {
+  const client = new QueryClient();
   return (
     <View style={styles.container}>
-      <AppNavigator />
+      <QueryClientProvider client={client}>
+        <AppNavigator />
+      </QueryClientProvider>
     </View>
   );
 }
