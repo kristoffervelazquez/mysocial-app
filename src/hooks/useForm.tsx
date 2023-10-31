89136857dd5e06047cf13ef8b0b15d5163e798e4
Kristoffer Velazquez <kristoffervelazquez49@live.com>
2023-10-31 02:11:31 -0700

    custom hook useForm

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+const useForm = <T extends Object>(form: T) => {
+  const [state, setState] = useState(form);
+
+  const onChange = <K extends Object>(value: K, campo: keyof T) => {
+    setState({
+      ...state,
+      [campo]: value,
+    });
+  };
+
+  return { ...state, formulario: state, onChange };
+};
+
+export default useForm;
